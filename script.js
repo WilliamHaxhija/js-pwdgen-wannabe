eee93cc644077b852881ec8203afec7495c5ffc3
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-05 00:50:03 +0100

    add bonus

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ const userName = prompt ('Il tuo nome');
 const userSurname = prompt ('Il tuo cognome');
 // Chiedo il colore preferito
 const userFavColor = prompt ('Il tuo colore preferito');
+// Bonus
+const userNumber = prompt ('In tuo numero preferito');
+const userNumberAsNumeric = parseInt(userNumber);
+const num23 = 23;
 // Creo la password
-const userPassword = userName + userSurname + userFavColor + 23;
+const numPassword = userNumberAsNumeric + num23;
+const userPassword = userName + userSurname + userFavColor + numPassword;
 // Creo il messaggio
 const userMessage = 'La tua password è: ' + userPassword;
 // Stampo il messaggio con la password
